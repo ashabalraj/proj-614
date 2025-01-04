870549e9d0743c8ef9d41a013925a10a02320253
ashabalraj <98542890+ashabalraj@users.noreply.github.com>
2025-01-04 23:42:12 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+  default="10.0.0.0/16"    
+}
+variable "publicsubnet1_cidr" {
+  default="10.0.1.0/24"    
+}
+variable "publicsubnet2_cidr" {
+  default="10.0.2.0/24"    
+}
+variable "privatesubnet1_cidr" {
+  default="10.0.3.0/24"    
+}
+variable "privatesubnet2_cidr" {
+  default="10.0.3.0/24"    
+}
